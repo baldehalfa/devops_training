033871c69102dd204a0617f30c2896774f662a48
Mamadou Alpha Balde <60265937+baldehalfa@users.noreply.github.com>
2022-05-01 14:27:27 +0800

    Created s3 bucket (#1)

--- terraform/terraform_basics/main.tf
+++ terraform/terraform_basics/main.tf
@@ -15,6 +15,13 @@ provider "aws" {
 # Creation of an S3 bucket
 resource "aws_s3_bucket" "my_s3_bucket" {
   bucket = "baldehalfa-s3-001"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+  name = "my_user_baldeh"
 }
 
 # Creation of an IAM user
