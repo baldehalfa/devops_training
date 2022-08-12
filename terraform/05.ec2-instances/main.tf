d4f10300146bdbf3abaa360f894607e74ec07595
baldehalfa <baldehalfa@yahoo.fr>
2022-08-12 18:09:01 +0800

    Adding terraform for creation of EC2 instances

--- terraform/05.ec2-instances/main.tf
+++ terraform/05.ec2-instances/main.tf
@@ -32,3 +32,29 @@ resource "aws_security_group" "http_server_sg" {
     name = "http_server_sg"
   }
 }
+
+resource "aws_instance" "http_server" {
+  #ami                   = "ami-062f7200baf2fa504"
+  ami                    = data.aws_ami.aws_linux_2_latest.id
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+
+  //subnet_id              = "subnet-3f7b2563"
+  subnet_id = data.aws_subnets.default_subnets.ids[0]
+
+#   connection {
+#     type        = "ssh"
+#     host        = self.public_ip
+#     user        = "ec2-user"
+#     private_key = file(var.aws_key_pair)
+#   }
+
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo yum install httpd -y",
+#       "sudo service httpd start",
+#       "echo Welcome to my server - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
+#     ]
+#   }
+}
