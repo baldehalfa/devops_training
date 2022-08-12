2cf44389cfe5ebdec90bf7750b7190a2d005018b
baldehalfa <baldehalfa@yahoo.fr>
2022-08-12 19:24:56 +0800

    Adding terraform for creation of ELB

--- terraform/06.ec2-elb/main.tf
+++ terraform/06.ec2-elb/main.tf
@@ -8,7 +8,7 @@ resource "aws_default_vpc" "default" {
 
 resource "aws_security_group" "http_server_sg" {
   name = "http_server_sg"
-  #   vpc_id = "vpc-c49ff1be"
+  //vpc_id = "vpc-c49ff1be"
   vpc_id = aws_default_vpc.default.id
 
   ingress {
@@ -34,6 +34,40 @@ resource "aws_security_group" "http_server_sg" {
 
   tags = {
     name = "http_server_sg"
+  }
+}
+
+resource "aws_security_group" "elb_sg" {
+  name   = "elb_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_elb" "elb" {
+  name            = "elb"
+  subnets         = data.aws_subnets.default_subnets.ids
+  security_groups = [aws_security_group.elb_sg.id]
+  instances       = values(aws_instance.http_servers).*.id
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
   }
 }
 
