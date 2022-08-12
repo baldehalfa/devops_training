9638127dd9b75f6d4d8d01b88392c28cf7df768e
baldehalfa <baldehalfa@yahoo.fr>
2022-08-12 16:22:36 +0800

    Adding terraform for lists and set

--- terraform/03.lists_and_sets/main.tf
+++ terraform/03.lists_and_sets/main.tf
@@ -10,5 +10,6 @@ provider "aws" {
 resource "aws_iam_user" "my_iam_users" {
   #count = length(var.names)
   #name  = var.names[count.index]
- 
+  for_each = toset(var.names)
+  name = each.value
 }
