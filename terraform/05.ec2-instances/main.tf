a3f875c8f0aa13739f3093eabe024cf6f34a7914
baldehalfa <baldehalfa@yahoo.fr>
2022-08-12 17:56:21 +0800

    Adding terraform for creation of EC2 instances

---
+++ terraform/05.ec2-instances/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = "vpc-c49ff1be"
+  #   vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+}
